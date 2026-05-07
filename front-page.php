7e5e5b45838d1712bd9012f8bf57277871e12269
Jameel Evans <jameelevans@MacBook-Pro.local>
2026-05-07 17:15:01 -0400

    Highlight Edits Left Side V1 May 7

--- front-page.php
+++ front-page.php
@@ -20,13 +20,13 @@ get_header();
 					<a href="" class="btn__outline-white">Who We Are</a>
 				</div>
 				<div class="about__image-wrap">
-					<img src="<?php echo get_stylesheet_directory_uri()?>/assets/img/police-with-kids.webp" alt="Police woman sitting with kids" class="about__image">
+					<div class="about__image top">&nbsp;</div>
 				</div>
 			</div>
 
 			<div class="about__container">
 				<div class="about__image-wrap">
-					<img src="<?php echo get_stylesheet_directory_uri()?>/assets/img/women-smiling-in-group.webp" alt="Polierferfwith kids" class="about__image">
+					<div class="about__image bottom">&nbsp;</div>
 				</div>
 				<div class="about__content">
 					<p class="about__subheader">CVIPI Across the Nation</p>
@@ -40,7 +40,198 @@ get_header();
 			
 		</section>
 		
-		<section class="">
+		<section class="highlights">
+			<div class="highlights__section">
+				<div class="highlights__container">
+					<div class="highlights__left">
+						<div class="highlights__top">
+							<header class="highlights__header">
+								<h2 class="highlights__heading">Upcoming Events</h2>
+							</header>
+							<a href="" class="highlights__link">View All <?php echo svg_icon('highlights__icon', 'arrow-right');?></a>
+						</div>
+						<div class="events">
+							<a href="" class="event">
+								<article class="event__container">
+									<time
+										class="event__date"
+										datetime="2026-05-07T14:00:00-04:00"
+										>
+										DATE May 07, 2026 · 2:00 PM ET
+									</time>
+									<header class="event__header">
+										<h3 class="event__heading">
+											Editorial Titles
+										</h3>
+									</header>
+
+									<p class="event__excerpt">
+										Forem ipsum dolor sit amet, consectetur adipiscing elit. Nunc vulputate libero et velit
+										interdum, ac aliquet odio mattis.
+									</p>
+
+									<ul class="event__tags" aria-label="Event tags">
+										<li class="event__tag">Webinar</li>
+										<li class="event__tag">Free</li>
+									</ul>
+								</article>
+							</a>
+
+							<a href="" class="event">
+								<article class="event__container">
+									<time
+										class="event__date"
+										datetime="2026-05-07T14:00:00-04:00"
+										>
+										DATE May 07, 2026 · 2:00 PM ET
+									</time>
+									<header class="event__header">
+										<h3 class="event__heading">
+											Editorial Titles
+										</h3>
+									</header>
+
+									<p class="event__excerpt">
+										Forem ipsum dolor sit amet, consectetur adipiscing elit. Nunc vulputate libero et velit
+										interdum, ac aliquet odio mattis.
+									</p>
+
+									<ul class="event__tags" aria-label="Event tags">
+										<li class="event__tag">Webinar</li>
+										<li class="event__tag">Free</li>
+									</ul>
+								</article>
+							</a>
+
+							<a href="" class="event">
+								<article class="event__container">
+									<time
+										class="event__date"
+										datetime="2026-05-07T14:00:00-04:00"
+										>
+										DATE May 07, 2026 · 2:00 PM ET
+									</time>
+									<header class="event__header">
+										<h3 class="event__heading">
+											Editorial Titles
+										</h3>
+									</header>
+
+									<p class="event__excerpt">
+										Forem ipsum dolor sit amet, consectetur adipiscing elit. Nunc vulputate libero et velit
+										interdum, ac aliquet odio mattis.
+									</p>
+
+									<ul class="event__tags" aria-label="Event tags">
+										<li class="event__tag">Webinar</li>
+										<li class="event__tag">Free</li>
+									</ul>
+								</article>
+							</a>
+
+					
+						</div>
+					</div>
+					<div class="highlights__right">
+						<div class="highlights__top">
+							<header class="highlights__header">
+								<h2 class="highlights__heading">Resources</h2>
+							</header>
+							<a href="" class="highlights__link">Browse Library <?php echo svg_icon('highlights__icon', 'arrow-right');?></a>
+						</div>
+						<div class="resources">
+							<a href="" class="resource">
+								<article class="resource__container">
+									<div class="resource__icon">
+										☹︎
+									</div>
+									<div class="resource__content">
+										<header class="resource__header">
+											<p class="resource__category">Toolkit</p>
+											<h3 class="resource__title">Title Here</h3>
+										</header>
+
+										<p class="resource__details">
+											<time class="resource__date" datetime="2026-05-07T14:00:00-04:00">
+												May 07, 2026 · 2:00 PM ET
+											</time> · 
+											<div class="resource__lenght">48 pages</div>
+										</p>
+									</div>
+									
+								</article>
+							</a>
+
+							<a href="" class="resource">
+								<article class="resource__container">
+									<div class="resource__icon">
+										☹︎
+									</div>
+									<div class="resource__content">
+										<header class="resource__header">
+											<p class="resource__category">Toolkit</p>
+											<h3 class="resource__title">Title Here</h3>
+										</header>
+
+										<p class="resource__details">
+											<time class="resource__date" datetime="2026-05-07T14:00:00-04:00">
+												May 07, 2026 · 2:00 PM ET
+											</time> · 
+											<div class="resource__lenght">48 pages</div>
+										</p>
+									</div>
+									
+								</article>
+							</a>
+
+							<a href="" class="resource">
+								<article class="resource__container">
+									<div class="resource__icon">
+										☹︎
+									</div>
+									<div class="resource__content">
+										<header class="resource__header">
+											<p class="resource__category">Toolkit</p>
+											<h3 class="resource__title">Title Here</h3>
+										</header>
+
+										<p class="resource__details">
+											<time class="resource__date" datetime="2026-05-07T14:00:00-04:00">
+												May 07, 2026 · 2:00 PM ET
+											</time> · 
+											<div class="resource__lenght">48 pages</div>
+										</p>
+									</div>
+									
+								</article>
+							</a>
+
+							<a href="" class="resource">
+								<article class="resource__container">
+									<div class="resource__icon">
+										☹︎
+									</div>
+									<div class="resource__content">
+										<header class="resource__header">
+											<p class="resource__category">Toolkit</p>
+											<h3 class="resource__title">Title Here</h3>
+										</header>
+
+										<p class="resource__details">
+											<time class="resource__date" datetime="2026-05-07T14:00:00-04:00">
+												May 07, 2026 · 2:00 PM ET
+											</time> · 
+											<div class="resource__lenght">48 pages</div>
+										</p>
+									</div>
+									
+								</article>
+							</a>
+						</div>
+					</div>
+				</div>
+				
+			</div>
 
 		</section>
 		<section class="">
